84bb0da4ca1d46590c3a1b1a59779cd18498aff8
Rahul Shinde <rshinde@mica.edu>
2016-12-09 17:39:58 -0500

    adding index to website

--- js/comissions.js
+++ js/comissions.js
@@ -2,8 +2,15 @@ $(document).ready( function(){
   var root_size = Math.sqrt($(window).height() * $(window).width()) * 0.017;
   $('html').css({ 'font-size': root_size });
 
+  $('.index_button').on('click', toggleIndex)
+
   $(window).resize(changeRootFontSize);
 })
+
+toggleIndex = function(){
+  $('#project_index').toggleClass('show');
+  $('.index_button').toggleClass('show');
+}
 
 changeRootFontSize = function(el) {
   root_size = Math.sqrt($(window).height() * $(window).width()) * 0.017;
